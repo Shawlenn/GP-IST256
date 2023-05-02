d9914aafe2b43e7b35d6af7257fce2987a1517b5
Shawlenn <131184669+Shawlenn@users.noreply.github.com>
2023-05-02 15:23:02 -0400

    Add files via upload

--- Home.js
+++ Home.js
@@ -3,17 +3,25 @@ import { Link } from 'react-router-dom';
 import BannerImage from '../assets/BackgroundIMG.jpg';
 import '../styles/Home.css';
 
+import MyCard from '../components/MyCard';
+
+
 function Home() {
   return (
     <div className='home'style={{ backgroundImage: `url(${BannerImage})`}}> 
         <div 
-            className='headerContainer'           >
+            className='headerContainer' >
             <h1>A'vere</h1>
             <p>Fashion For Any Taste</p>
             <Link to='/product'>
                 <button>Order Now</button>
             </Link>
+            
+
+            
+            
         </div>
+       
     </div>
   );
 }
