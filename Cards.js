3736bcd3635275d954e7ed88aeaab29ec3150123
Shawlenn <131184669+Shawlenn@users.noreply.github.com>
2023-05-02 19:56:08 -0400

    Add files via upload

--- Cards.js
+++ Cards.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import '../styles/Cards.css'
 import CardItem from './CardItem';
+import CardPic from '../assets/1.png';
+import CardPic2 from '../assets/4.png';
+import CardPic3 from '../assets/2.png';
+
 
 function Cards() {
   return (
-    <div className='cards'>
-      <h2>HOT PRODUCTS!</h2>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='assets/1.png'
-              text='Womens full sleeve Black dress'
-              label='Casual'
-              path='/Product'
-            />
-            <CardItem
-              src='assets/4.png'
-              text='Womens Brown Suit pant'
-              label='Casual'
-              path='/Product'
-            />
-          </ul>
-          
+    <div className='wrapper'>
+      <h2>Hot Products!!!</h2>
+      
+      <div className='card'>
+        <div className='card__body'>
+          <img src= {CardPic} class="card__image" />
+          <h2 className='card__title'>Womens' Dress</h2>
+          <p className='card__description'>Long Sleeve 
+          dress for casual wear</p>
         </div>
+        <button className='card__btn'>Shop Here</button>
+
+      </div>
+
+      <div className='card'>
+        <div className='card__body'>
+          <img src= {CardPic2} class="card__image" />
+          <h2 className='card__title'>Womens' suit Pant</h2>
+          <p className='card__description'>Suit Pant
+           for casual wear</p>
+        </div>
+        <button className='card__btn'>Shop Here</button>
+
+      </div>
+
+      <div className='card'>
+        <div className='card__body'>
+          <img src= {CardPic3} class="card__image" />
+          <h2 className='card__title'>Womens' Dress</h2>
+          <p className='card__description'> 
+          dress for casual wear</p>
+        </div>
+        <button className='card__btn'>Shop Here</button>
+
       </div>
     </div>
   );
